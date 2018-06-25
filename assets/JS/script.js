945a4643bb048b41978967f3364d6f9a1baeee7f
dembasy <dembasy1227@gmail.com>
2018-06-25 12:02:22 +0200

    Fin de l'exercice2

---
+++ assets/JS/script.js
@@ -0,0 +1,6 @@
+function showText(){
+  var showText = document.getElementById('text').style.visibility = 'visible';
+}
+function hideText() {
+    var hideText = document.getElementById('text').style.visibility = 'hidden';
+}
